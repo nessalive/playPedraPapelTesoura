00f9fe8203b3038db10e45efa27d0cc9de6deda2
alisonmelo <61011223+alisonmelo@users.noreply.github.com>
2020-08-01 01:30:06 -0300

    Update index.js
    toda a interatividade do jogo em js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
 let escolhaJogador
+let score = document.querySelector("h1#pontos")
+let valorScore = 0
+let vida = document.querySelector("h1#vida")
+let valorVida = 3
 
 function selecao(escolha) {
 
@@ -11,13 +15,107 @@ function selecao(escolha) {
     } else {
         escolhaJogador = "tesoura"
     }
-    window.alert(`${escolhaJogador}`)
+    /*   window.alert(`${escolhaJogador}`) */
 }
 
 function jogar() {
-    let escolhaComputador = Math.floor(Math.random() * 3)
-    alert(escolhaComputador)
-    if (escolhaJogador === "pedra" && escolhaComputador === 0) {
 
+    let escolhaComputador = Math.floor(Math.random() * (3 - 0) - 0)
+    if (escolhaComputador == 0) {
+        escolhaComputador = "pedra"
+    } else if (escolhaComputador == 1) {
+        escolhaComputador = "papel"
+    } else {
+        escolhaComputador = "tesoura"
     }
+
+    if (escolhaJogador === "pedra") {
+        if (escolhaComputador === "pedra") {
+            alert(`empate`)
+        } else if (escolhaComputador === "tesoura") {
+            alert(`Jogador venceu`)
+            valorScore++
+        } else {
+            alert(`Jogador Perdeu`)
+            valorVida--
+        }
+    } else if (escolhaJogador === "tesoura") {
+        if (escolhaComputador === "pedra") {
+            alert(`Jogador Perdeu`)
+            valorVida--
+        } else if (escolhaComputador === "tesoura") {
+            alert(`jogador Ganhou`)
+            valorScore++
+        } else {
+            alert(`Empate`)
+        }
+    } else if (escolhaJogador === "papel") {
+        if (escolhaComputador === "tesoura") {
+            alert(`ganhou`)
+            valorScore++
+        } else if (escolhaComputador === "papel") {
+            alert(`empate`)
+        } else {
+            alert(`perdeu`)
+            valorVida--
+        }
+    }
+
+    if (valorVida === 0) {
+        alert(`Se ferrou`)
+        valorVida = 3
+        valorScore = 0
+    }
+    score.innerHTML = ` Belezura de Pontos: ${valorScore}`
+    vida.innerHTML = `Vida: ${valorVida}`
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+if (escolhaJogador === "pedra" && escolhaComputador === "pedra") {
+    alert(`empate`)
+} else if (escolhaJogador === "papel" && escolhaComputador === "papel") {
+    alert(`empate`)
+
+} else if (escolhaJogador === "tesoura" && escolhaComputador === "tesoura") {
+    alert(`empate`)
+
+} else if (escolhaJogador === "papel" && escolhaComputador === "tesoura") {
+    alert(`vc perdeu`)
+
+} else if (escolhaJogador === "pedra" && escolhaComputador === "tesoura") {
+    alert(`vc ganhou`)
+
+} else if (escolhaJogador === "pedra" && escolhaComputador === "papel") {
+    alert(`vc ganhou`)
+
+} else if (escolhaJogador === "papel" && escolhaComputador === "pedra") {
+    alert(`vc ganhou`)
+
+} else if (escolhaJogador === "tesoura" && escolhaComputador === "papel") {
+    alert(`vc ganhou`)
+
+} else if (escolhaJogador === "tesoura" && escolhaComputador === "pedra") {
+    alert(`vc perdeu`)
+} */
